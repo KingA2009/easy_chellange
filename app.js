787bf1f715a7ffa63b2dde43b15b1929518ffe1c
KingA2009 <ajamoliddinov12@gmail.com>
2023-07-28 20:21:16 +0500

    fix

---
+++ app.js
@@ -0,0 +1,5 @@
+let name = prompt("Saytda ro`yxatdan otish uchun ismingizni kiriting")
+alert("Siz saytimizdan muvofaqqiyatli ro`yhatdan o`tdingiz!")
+function onclick_f() {
+        document.querySelector(".name").innerHTML = "Ismingiz: " + name + " To`g'ri topdimmi?"        
+}
